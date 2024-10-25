51c6a075314aff784ef04b1f2bf1f4a536bf18e2
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2024-10-25 16:03:00 +0200

    Changed theme to onedark

--- lua/ouailderghal/plugins/theme.lua
+++ lua/ouailderghal/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-	"bluz71/vim-moonfly-colors",
+	"navarasu/onedark.nvim",
 	lazy = false,
 	priority = 1000,
 	cond = true,
 	config = function()
-		vim.cmd([[ colorscheme moonfly ]])
+		vim.cmd([[ colorscheme onedark ]])
 	end,
 }
